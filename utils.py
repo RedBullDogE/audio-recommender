29d433e00d852353a5832ff6dca937311867cd73
Drew Kryvonos <gsb06092000@gmail.com>
2021-06-05 13:19:37 +0300

    Add utils file

--- utils.py
+++ utils.py
@@ -6,3 +6,18 @@ def to_1D(arr_2d):
     Функція-утиліта для перетворення двомірного масиву у одномірний
     """
     return pd.Series([x for _list in arr_2d for x in _list])
+
+
+def normalize(df, cols=None):
+    """
+    Функція-утиліта для нормалізації даних стовпців вказаного датафрейму
+    """
+    result = df.copy()
+    if cols is None:
+        cols = df.columns
+
+    for feature_name in cols:
+        max_value = df[feature_name].max()
+        min_value = df[feature_name].min()
+        result[feature_name] = (df[feature_name] - min_value) / (max_value - min_value)
+    return result
